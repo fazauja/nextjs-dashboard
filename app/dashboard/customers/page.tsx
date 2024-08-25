f8f2fff7fea0a29451c3cef5ad74102ef36f0341
fazauja <faza150799@gmail.com>
2024-08-25 20:04:02 +0700

    learn nextjs

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CustomersPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Halaman Pelanggan</h1>
+            <p>Daftar pelanggan akan ditampilkan di sini.</p>
+            {/* Tambahkan komponen atau logika untuk menampilkan daftar pelanggan */}
+        </div>
+    );
+};
+
+export default CustomersPage;
